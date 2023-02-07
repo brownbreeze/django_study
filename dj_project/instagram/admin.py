e460b643147e0ea7f5bf18374a834d8c659372a7
brownbreeze <913breeze@gmail.com>
2023-02-07 05:58:49 +0000

    Study : django models db 처리
    - admin page 변경(PostAdmin)
    - instagram 모델 구조 변경

--- dj_project/instagram/admin.py
+++ dj_project/instagram/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# 1 
+#admin.site.register(Post)
+
+# 2
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Post, PostAdmin)
+
+# 3
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message'] # link 설정
+    search_fields = ['message'] # 검색 쉽게 가능 
+    list_filter = ['created_at', 'is_public'] # 데이터베이스 창 오른쪽에 필터 정보
+
+    def message_length(self, post):
+        return f'{len(post.message)} 글자'
--- dj_project/instagram/models.py
+++ dj_project/instagram/models.py
@@ -2,5 +2,17 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # java의 toString
+    def __str__(self):
+        #return f'Custom Post object({self.id})'
+        return self.message
+
+    def message_length(self):
+        return len(self.message)
+    # model 단에서 구현하느나, admin 단에서 구현하느냐..? 
+    # 자주 쓰이는 것은 model 단이 맞을 것 같다.
+    message_length.short_description = '메시지 글자수'
