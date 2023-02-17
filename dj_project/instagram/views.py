c480d68c29b4c807e67b6814294a10bd5fd531c1
brownbreeze <913breeze@gmail.com>
2023-02-17 06:13:51 +0000

    Study : django models db 처리
    - instagram 화면 반영

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # request.GET
+    # request.REQUEST
+    # request.POST
+    qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+    # instagram/templates/instagram/post_list.html 를 암시
+    return render(request, 'instagram/post_list.html',{
+        'post_list' : qs,
+
+    })
