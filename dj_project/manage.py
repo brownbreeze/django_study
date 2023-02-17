851be0e601a0f9b90a92509d26d48e84e9f1a817
brownbreeze <913breeze@gmail.com>
2023-02-17 05:45:02 +0000

    Study : django models db 처리
    - query set 설명 주석처리

--- dj_project/manage.py
+++ dj_project/manage.py
@@ -25,3 +25,49 @@ if __name__ == '__main__':
     >>> Post.objects.all()
     <QuerySet [<Post: 첫번째 메시지>, <Post: 두번째 메시지>, <Post: 세번째 메시지>]>
 '''
+
+'''
+# 모델을 통한 조회(기초)
+>>> qs = Post.objects.all().order_by('-id')
+>>> qs
+<QuerySet [<Post: 세번째 메시지>, <Post: 두번째 메시지>, <Post: 첫번째 메시지>]>
+>>> print(qs.query)
+SELECT "instagram_post"."id", "instagram_post"."message", "instagram_post"."photo", "instagram_post"."is_public", "instagram_post"."created_at", "instagram_post"."updated_at" FROM "instagram_post" ORDER BY "instagram_post"."id" DESC
+>>> for post in qs:
+...     print("id:{id}, mag:{message} ({created_at})".format(**post.__dict__))
+...
+id:3, mag:세번째 메시지 (2023-02-07 05:28:27.637628+00:00)
+id:2, mag:두번째 메시지 (2023-02-07 05:28:22.679304+00:00)
+id:1, mag:첫번째 메시지 (2023-02-07 05:28:17.466399+00:00)
+>>> qs = Post.objects.all().filter(message='첫번째 메시지')
+>>> qs
+<QuerySet [<Post: 첫번째 메시지>]>
+>>> qs = Post.objects.all().filter(message__startswith='첫번째')
+>>> qs
+<QuerySet [<Post: 첫번째 메시지>]>
+>>> qs = Post.objects.all().filter(message__icontains='첫번째')
+>>> qs
+<QuerySet [<Post: 첫번째 메시지>]>
+>>> qs.none()
+<QuerySet []>
+>>> qs.first()
+<Post: 첫번째 메시지>
+>>> qs.last()
+<Post: 첫번째 메시지>
+>>> qs.none().last()
+
+
+>>> from django.db.models import Q
+>>> qs = Post.objects.all()
+>>> qs = qs.exclude(id__gte=2, message__icontains=query)
+>>> print(qs.query)
+SELECT "instagram_post"."id", "instagram_post"."message", "instagram_post"."photo", "instagram_post"."is_public", "instagram_post"."created_at", "instagram_post"."updated_at" FROM "instagram_post" WHERE NOT ("instagram_post"."id" >= 2 AND "instagram_post"."message" LIKE %첫번째% ESCAPE '\')
+>>> qs = Post.objects.all()
+>>> qs = qs.exclude(Q(id__gte=2) & Q(message__icontains=query))
+>>> print(qs.query)
+SELECT "instagram_post"."id", "instagram_post"."message", "instagram_post"."photo", "instagram_post"."is_public", "instagram_post"."created_at", "instagram_post"."updated_at" FROM "instagram_post" WHERE NOT ("instagram_post"."id" >= 2 AND "instagram_post"."message" LIKE %첫번째% ESCAPE '\')
+>>> qs = Post.objects.all()
+>>> qs = qs.exclude(Q(id__gte=2) |  Q(message__icontains=query))
+>>> print(qs.query)
+SELECT "instagram_post"."id", "instagram_post"."message", "instagram_post"."photo", "instagram_post"."is_public", "instagram_post"."created_at", "instagram_post"."updated_at" FROM "instagram_post" WHERE NOT (("instagram_post"."id" >= 2 OR "instagram_post"."message" LIKE %첫번째% ESCAPE '\'))
+'''
