9fe60fa1fb2637bc1d587071193c5eab5f2aebcd
brwonbreeze <913breeze@gmail.com>
2023-02-21 14:37:22 +0000

    Study : Views - HTTP
    - django.http 활용

--- dj_project/instagram/urls.py
+++ dj_project/instagram/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path, include #, re_path
 from . import views
+
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
+#    re_path(r'(?P<pk>\)/$', views.post_detail),
 ]
--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -1,5 +1,10 @@
+# from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse, HttpRequest
+
+# view 기준이나, 이는 function 을 더 익숙하게 한 후 사용 권장 
+# post_list = ListView.as_view(model=Post)
 
 def post_list(request):
     # request.GET
@@ -14,3 +19,11 @@ def post_list(request):
         'post_list' : qs,
         'q' : q,
     })
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse: 
+    #eturn render(request, 'instagram/post_list.html')
+    # request.method, META, GET, POST, FILES, body
+    response = HttpResponse()
+    response.write("hello world")
+    response.write("hello world")
+    return response
