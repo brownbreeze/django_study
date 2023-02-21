13d7ac1409150ca39a9069d3e5dff677983667ae
brwonbreeze <913breeze@gmail.com>
2023-02-21 15:05:34 +0000

    Study : Views - HTTP
    - url regex 상세 적용

--- dj_project/instagram/urls.py
+++ dj_project/instagram/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include #, re_path
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list),
     path('<int:pk>/', views.post_detail),
-    path('archives/<int:year>/', views.archives_year),
+    # path('archives/<int:year>/', views.archives_year),
+    # 특정 연도 또는 4자리 숫자만 허용하고 싶을 경우,
+    # ?P python re 작성법
+    re_path(r'archives/(?P<year>\d{4})/', views.archives_year),
 ]
