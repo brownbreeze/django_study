22edf19c6b016bfb6e41fc6ec89f36853033d73b
brwonbreeze <913breeze@gmail.com>
2023-02-22 13:54:28 +0000

    Study : Views - HTTP
    - class based view 예제

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import HttpResponse, HttpRequest#, Http404
+from django.views.generic import DetailView
 
 # view 기준이나, 이는 function 을 더 익숙하게 한 후 사용 권장 
 # post_list = ListView.as_view(model=Post)
@@ -20,11 +21,13 @@ def post_list(request):
         'q' : q,
     })
 
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse: 
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'instagram/post_detail.html',{
-        'post':post,
-    })
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse: 
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'instagram/post_detail.html',{
+#         'post':post,
+#     })
+
+post_detail = DetailView.as_view(model=Post)
 
 def archives_year(request, year):
     return HttpResponse(f"{year}년")
