7e0a4d160faddac3aedcf83a0bc4e8179d1310e1
brwonbreeze <913breeze@gmail.com>
2023-02-23 15:00:15 +0000

    Study : Views - HTTP
    - ListView Page

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -2,24 +2,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import HttpResponse, HttpRequest#, Http404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 # view 기준이나, 이는 function 을 더 익숙하게 한 후 사용 권장 
-# post_list = ListView.as_view(model=Post)
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
-def post_list(request):
-    # request.GET
-    # request.REQUEST
-    # request.POST
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html 를 암시
-    return render(request, 'instagram/post_list.html',{
-        'post_list' : qs,
-        'q' : q,
-    })
+# def post_list(request):
+#     # request.GET
+#     # request.REQUEST
+#     # request.POST
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html 를 암시
+#     return render(request, 'instagram/post_list.html',{
+#         'post_list' : qs,
+#         'q' : q,
+#     })
 
 # def post_detail(request: HttpRequest, pk: int) -> HttpResponse: 
 #     post = get_object_or_404(Post, pk=pk)
