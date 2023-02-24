8f1d063278d5cbc832d484930ea121f14d2b4f9a
brwonbreeze <913breeze@gmail.com>
2023-02-24 14:53:48 +0000

    Study : Views - HTTP
    - Converter 생성
    - 활용url 은추후 개발

--- dj_project/instagram/urls.py
+++ dj_project/instagram/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path, include, re_path, register_converter
 from . import views
-
-class YearConverter:
-    regex = r"20\d{2}"
-
-    def to_python(self, value):
-        return int(value)
-
-    def to_url(self, value):
-        return str(value)
-
+from .converters import YearConverter, MonthConverter, DayConverter
 register_converter(YearConverter, 'year')
+register_converter(MonthConverter, 'month')
+register_converter(DayConverter, 'day')
 
 app_name = 'instagram' # URL Reverse 에서 namespace 역할 
 
@@ -20,5 +13,7 @@ urlpatterns = [
     # path('archives/<int:year>/', views.archives_year),
     # path('archives/<year:year>/', views.archives_year),
     path('archive/', views.post_archive, name='post_archive'),
-    path('archive/<year:year>/', views.post_archive_year, name='post_archive_year')
+    path('archive/<year:year>/', views.post_archive_year, name='post_archive_year'),
+    # path('archive/<year:year>/<month:month>', views.post_archive_month, name='post_archive_month'),
+    # path('archive/<year:year>/<month:month>/<day:day>', views.post_archive_day, name='post_archive_day'),
 ]
