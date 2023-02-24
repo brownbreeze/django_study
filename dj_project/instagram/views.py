19a969626f43e57cac904de3091827f94cacbe66
brownbreeze <913breeze@gmail.com>
2023-02-24 01:15:01 +0000

    Study : Views - HTTP
    - method_decorator
    - CBV에 장식자 입히기
    - LoginRequiredMixin 사용, dispatch에 method_decorator 사용

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -1,16 +1,26 @@
-# from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import HttpResponse, HttpRequest#, Http404
+from django.http import HttpResponse, HttpRequest
 from django.views.generic import DetailView, ListView
 
 # view 기준이나, 이는 function 을 더 익숙하게 한 후 사용 권장 
-post_list = ListView.as_view(model=Post, paginate_by=10)
+# post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
+# 요즘방식
+#class PostListView(LoginRequiredMixin, ListView):
+# dispatch 란 class 기반 뷰에서 실제 요청왔을 때, 
+# 어떤 request method function이 호출된다 하더라도 호출되는 function
+@method_decorator(login_required, name='dispatch')
+class PostListView(ListView):
+    model = Post
+    paginate_by = 10
 
+post_list = PostListView.as_view()
+
+# @login_required
 # def post_list(request):
-#     # request.GET
-#     # request.REQUEST
-#     # request.POST
 #     qs = Post.objects.all()
 #     q = request.GET.get('q', '')
 #     if q:
