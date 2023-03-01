d08e3b5e403f326082ada6ba87dd967c136370e8
brwonbreeze <913breeze@gmail.com>
2023-03-01 11:58:58 +0000

    Study : Web Frontend - static with django
    -

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.utils.decorators import method_decorator
+from django.views.generic import ArchiveIndexView, ListView, YearArchiveView
+
 from .models import Post
-from django.http import HttpResponse, HttpRequest
-from django.views.generic import DetailView, ListView, ArchiveIndexView, YearArchiveView
+
 
 # view 기준이나, 이는 function 을 더 익숙하게 한 후 사용 권장 
 # post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
 # 요즘방식
-#class PostListView(LoginRequiredMixin, ListView):
+#class PostListVipew(LoginRequiredMixin, ListView):
 # dispatch 란 class 기반 뷰에서 실제 요청왔을 때, 
 # 어떤 request method function이 호출된다 하더라도 호출되는 function
 @method_decorator(login_required, name='dispatch')
@@ -39,6 +41,7 @@ post_list = PostListView.as_view()
 
 # 관례에 맞게 개발할경우, 이렇게 소스 양이 작아짐 
 post_detail = DetailView.as_view(model=Post)
+
 
 # def archives_year(request, year):
 #     return HttpResponse(f"{year}년")
