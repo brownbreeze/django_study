b7a07ac2cc957a4815ca363907c16040f7a28804
brwonbreeze <913breeze@gmail.com>
2023-03-11 16:06:41 +0000

    Study : django Forms
    - clean_필드명 유효성검사 방식

--- dj_project/instagram/forms.py
+++ dj_project/instagram/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from .models import Post
 
@@ -9,3 +10,9 @@ class PostForm(forms.ModelForm):
             'message','photo','tag_set','is_public'
         ]
         # exclude = [ ] # 비추
+    #   def clean() # 두개 이상의 유형에 대한 유효성 검사가 필요할 때 -> Form객체.clean()
+    def clean_message(self): # 하나에 대한 유효성 검사가 필요할 때 
+        message = self.cleaned_data.get('message')
+        if message:
+            message = re.sub(r'[a-zA-Z]+', '', message)
+        return message
