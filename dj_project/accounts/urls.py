f792b7f560fccc7142a7ec69695cbe4996bf141a
brwonbreeze <913breeze@gmail.com>
2023-03-12 15:23:31 +0000

    Study : django basic accounts
    - accounts/profile 화면 기본 구성
    - urls 파일 위치 오류 수정

--- dj_project/accounts/urls.py
+++ dj_project/accounts/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views #from accounts import views
+
 urlpatterns = [
-    
+    path('login', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('profile/', views.profile, name='profile')
 ]
--- dj_project/dj_project/templates/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),  
-]
