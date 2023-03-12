d4590a78fa32c7a57d643ba86b63c3171db3c4cc
brwonbreeze <913breeze@gmail.com>
2023-03-12 14:48:30 +0000

    Study : django Forms
    - 오류 수정 (import 오류)

--- dj_project/instagram/views.py
+++ dj_project/instagram/views.py
@@ -4,53 +4,74 @@ from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView, ArchiveIndexView, ListView, YearArchiveView
-from .forms import PostForm
+from django.views.generic import DetailView, ArchiveIndexView, ListView, YearArchiveView, CreateView, UpdateView, DeleteView
+from .forms import PostForm 
 from .models import Post
 
-@login_required
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid(): # 유효성 검증 
-            # post = form.save(commit=True) # db에 저장해줌
-            post = form.save(commit=False) # db에 저장해줌
-            post.author = request.user # 현재 로그인 user instance 인증은 아직 안함 
-            post.save()
-            messages.success(request, '포스팅을 추가했습니다.')
-            # commit=False를 한다면, db에 생성이 안됨 
-            # 위 항목은 instance.save()를 지연시키고자 할 때 사용 
-            return redirect(post)
-    else:
-        form = PostForm()
+# @login_required
+# def post_new(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid(): # 유효성 검증 
+#             # post = form.save(commit=True) # db에 저장해줌
+#             post = form.save(commit=False) # db에 저장해줌
+#             post.author = request.user # 현재 로그인 user instance 인증은 아직 안함 
+#             post.save()
+#             messages.success(request, '포스팅을 추가했습니다.')
+#             # commit=False를 한다면, db에 생성이 안됨 
+#             # 위 항목은 instance.save()를 지연시키고자 할 때 사용 
+#             return redirect(post)
+#     else:
+#         form = PostForm()
         
-    return render(request, 'instagram/post_form.html',{
-        'form':form,        
-    })
+#     return render(request, 'instagram/post_form.html',{
+#         'form':form,        
+#     })
 
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+post_edit = PostUpdateView.as_view()
+
+#@login_required
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        messages.success(self.request, '포스팅을 저장했습니다.')
+        
+        return super().form_valid(form)
+
+post_new = PostCreateView.as_view()
+
+# @login_required
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
     
-    # 작성자 check tip 
-    if post.author != request.user :
-        # 장식자를 이용해서 적용 가능 
-        messages.error(request, '작성자만 수정할 수 있습니다.')
-        return redirect(post) 
+#     # 작성자 check tip 
+#     if post.author != request.user :
+#         # 장식자를 이용해서 적용 가능 
+#         messages.error(request, '작성자만 수정할 수 있습니다.')
+#         return redirect(post) 
 
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid(): # 유효성 검증 
-            post = form.save()
-            messages.success(request, '포스팅을 수정했습니다.')
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES, instance=post)
+#         if form.is_valid(): # 유효성 검증 
+#             post = form.save()
+#             messages.success(request, '포스팅을 수정했습니다.')
 
-            return redirect(post)
-    else:
-        form = PostForm(instance=post)
+#             return redirect(post)
+#     else:
+#         form = PostForm(instance=post)
         
-    return render(request, 'instagram/post_form.html',{
-        'form':form,        
-    })
+#     return render(request, 'instagram/post_form.html',{
+#         'form':form,        
+#     })
     
 
 @login_required
@@ -98,6 +119,13 @@ post_list = PostListView.as_view()
 #         'post':post,
 #     })
 
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model =Post 
+    success_url = reversed('instagrem')
+    
+    def get_success_url(self):
+        return reversed('instagram:post_list')
+     
 # 관례에 맞게 개발할경우, 이렇게 소스 양이 작아짐 
 post_detail = DetailView.as_view(model=Post)
 
